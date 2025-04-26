d9aee039182b9f3ccd4bdb81960d46d12d54b491
linhle15-ll <le224l@mtholyoke.edu>
2025-04-25 23:21:33 -0400

    Add TypeScript config for server, fix MongoDB connection error

--- server/src/db/db.ts
+++ server/src/db/db.ts
@@ -1,13 +1,14 @@
-const mongoose = require('mongoose');
+import mongoose from "mongoose";
 
 const db = async () => {
   try {
     mongoose.set('strictQuery', false);
-    await mongoose.connect(process.env.MONGODB_URL);
+    await mongoose.connect(process.env.MONGODB_URL as string || '');
+  
     console.log('Connected to MongoDB');
   } catch (error) {
     console.error('Failed to connect to MongoDB', error);
   }
 };
 
-module.exports = db;
+export default db;
---
+++ server/src/server.ts
@@ -0,0 +1,19 @@
+// Import required modules
+import http from 'http';
+import dotenv from 'dotenv';
+// Load environment variables (Jote: dotenv.config() must be top of the file to be activated correctly)
+dotenv.config();
+
+import app from './app';
+
+
+
+// Define the port
+const PORT = process.env.PORT || 5001; // if there is no port, return port 5000
+
+// create server instance
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}.`)
+});
