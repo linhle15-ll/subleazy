69fab520004230c08b3033b56a1637a2fe0fb5ad
linhle15-ll <le224l@mtholyoke.edu>
2025-04-27 02:53:16 -0400

    Changes on request by reviewer

--- server/src/db/db.ts
+++ server/src/db/db.ts
@@ -3,7 +3,11 @@ import mongoose from "mongoose";
 const db = async () => {
   try {
     mongoose.set('strictQuery', false);
-    await mongoose.connect(process.env.MONGODB_URL as string || '');
+    await mongoose.connect(process.env.MONGODB_URL as string);
+
+    if (process.env.NODE_ENV === 'development') {
+      mongoose.set('debug', true);
+    }
   
     console.log('Connected to MongoDB');
   } catch (error) {
