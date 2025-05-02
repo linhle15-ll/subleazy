690b7c2da3dba26b723596c4ffbb65d35a67f043
Duc Nguyen <ngmductm@gmail.com>
2025-05-02 16:00:52 -0400

    rename and fix import for shadcn utils and constants

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { ACCESS_TOKEN } from '@/lib/utils/constants';
+import { ACCESS_TOKEN } from '@/lib/constants';
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
