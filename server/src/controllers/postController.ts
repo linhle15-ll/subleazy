feb98c75b201bdeccb14a0151d38cff91914470d
Duc Nguyen <ngmductm@gmail.com>
2025-05-04 01:02:08 -0400

    add validators and controller for creating posts

---
+++ server/src/controllers/postController.ts
@@ -0,0 +1,52 @@
+import { Request, Response } from 'express';
+import houseService from '../services/houseService';
+import postService from '../services/postService';
+import { House } from '../types/houseType';
+import { PostRequestBody } from '../types/postType';
+import { Types } from 'mongoose';
+import { validateMedia, validateTime } from '../utils/validationUtils';
+// import { getAuthRequest } from "../utils/commonUtils";
+
+const postController = {
+  createPost: async (req: Request, res: Response) => {
+    try {
+      // TODO: Use authReq after jwt token is implemented
+      // const authReq = getAuthRequest(req);
+      // const data: PostRequestBody = authReq.body;
+      // data.author = new Types.ObjectId(authReq.user.id);
+
+      const data: PostRequestBody = req.body;
+      data.author = new Types.ObjectId(req.body.user.id as string);
+
+      if (
+        !validateMedia(data.media) ||
+        (data.availability.checkinTime &&
+          !validateTime(data.availability.checkinTime)) ||
+        (data.availability.checkoutTime &&
+          !validateTime(data.availability.checkoutTime))
+      ) {
+        res.status(400).json({ error: 'Invalid data' });
+        return;
+      }
+
+      const houseData: House = {
+        address: data.address,
+        city: data.city,
+        state: data.state,
+        zip: data.zip,
+        lat: data.lat,
+        long: data.long,
+      };
+      const house = await houseService.getOrCreateHouse(houseData);
+      data.house = house._id;
+
+      const post = await postService.createPost(data);
+
+      res.status(201).json(post);
+    } catch (error) {
+      throw error;
+    }
+  },
+};
+
+export default postController;
