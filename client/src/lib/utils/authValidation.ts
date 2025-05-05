33eecb6e8c86626c5cd2eb43229bc6b3f2defd96
xuananh-tran <189546946+xuananh-tran@users.noreply.github.com>
2025-05-05 00:07:17 -0700

    Create a login and signup form with validation

---
+++ client/src/lib/utils/authValidation.ts
@@ -0,0 +1,37 @@
+//TODO: validate academic email
+
+export const checkPasswordStrength = (password: string) => {
+  const error: string[] = [];
+
+  if (password.length < 8) {
+    error.push('Password must be at least 8 characters long.');
+  }
+
+  if (!/[a-z]/.test(password)) {
+    error.push('Password must contain at least one lowercase letter.');
+  }
+
+  if (!/[A-Z]/.test(password)) {
+    error.push('Password must contain at least one uppercase letter.');
+  }
+
+  if (!/[0-9]/.test(password)) {
+    error.push('Password must contain at least');
+  }
+
+  if (!/[@$!%*?&]/.test(password)) {
+    error.push('Password must contain at least one special character.');
+  }
+
+  return error;
+};
+
+export const validateConfirmPassword = (
+  password: string,
+  confirmPassword: string
+) => {
+  if (password != confirmPassword) {
+    return 'Passwords must match.';
+  }
+  return '';
+};
