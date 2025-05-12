7cd5ba19ff08c4867d3b0d1a906f25de8466f9ab
Duc Nguyen <ngmductm@gmail.com>
2025-05-12 11:23:27 -0500

    add controller function and set up route for search

--- server/src/controllers/post.controller.ts
+++ server/src/controllers/post.controller.ts
@@ -22,8 +22,8 @@ const postController = {
         !validateMedia(data.media) ||
         !validateTime(data.availability.checkinTime as string) ||
         !validateTime(data.availability.checkoutTime as string) ||
-        !validateTime(data.rules.quietHours?.from as string) ||
-        !validateTime(data.rules.quietHours?.to as string)
+        !validateTime(data.rules?.quietHours?.from as string) ||
+        !validateTime(data.rules?.quietHours?.to as string)
       ) {
         res.status(400).json({ error: 'Invalid data' });
         return;
@@ -43,6 +43,22 @@ const postController = {
       const post = await postService.createPost(data);
 
       res.status(201).json(post);
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  searchPosts: async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const data: Partial<PostRequestBody> = req.body;
+      if (!data.zip && !data.state && !data.lat && !data.long) {
+        res.status(400).json({ error: 'Missing location' });
+        return;
+      }
+
+      const posts = await postService.searchPosts(data);
+
+      res.status(200).json(posts);
     } catch (error) {
       next(error);
     }
