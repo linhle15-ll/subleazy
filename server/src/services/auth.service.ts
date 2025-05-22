6623c1be9d1534b3f8aa94f7a177af494dfbf375
xuananh-tran <189546946+xuananh-tran@users.noreply.github.com>
2025-05-21 17:18:43 -0700

    Add compare passwords function to auth.service.ts

--- server/src/services/auth.service.ts
+++ server/src/services/auth.service.ts
@@ -24,6 +24,13 @@ const authService = {
   hashPassword: async (password: string): Promise<string> => {
     return await bcrypt.hash(password, 10);
   },
+
+  comparePassword: async (
+    plainPassword: string,
+    hashedPassword: string
+  ): Promise<boolean> => {
+    return await bcrypt.compare(plainPassword, hashedPassword);
+  },
 };
 
 export default authService;
