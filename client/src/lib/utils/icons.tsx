e7b36c8a266d5f6cbfde5c76eda5fa4365c6bea0
Ngoc Linh Le <144966197+linhle15-ll@users.noreply.github.com>
2025-05-26 17:57:48 -0400

    Update icons.tsx

--- client/src/lib/utils/icons.tsx
+++ client/src/lib/utils/icons.tsx
@@ -1,20 +1,26 @@
-import { Home, User, Users } from 'lucide-react';
-
-export enum PlaceType {
-  ENTIRE = 'Entire',
-  PRIVATE = 'Private',
-  SHARED = 'Shared',
-}
+import { Home, User, Users, Building } from 'lucide-react';
+import { PlaceType, HouseType } from '@/lib/types/enums';
 
 export const getPlaceTypeIcon = (type: PlaceType) => {
   switch (type) {
     case PlaceType.ENTIRE:
       return Home;
-    case PlaceType.PRIVATE:
+    case PlaceType.PRV:
       return User;
     case PlaceType.SHARED:
       return Users;
     default:
-      return Home; // Default to Home icon
+      return Home; 
   }
 };
+
+export const getHouseTypeIcon = (type: HouseType) => {
+  switch (type) {
+    case HouseType.HOUSE:
+      return Home;
+    case HouseType.APT:
+      return Building; 
+    default:
+      return Home; 
+  }
+}
