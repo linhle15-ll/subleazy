0c5b36feb051810f37b6c2f11856f28b2478f9e7
linhle15-ll <le224l@mtholyoke.edu>
2025-06-04 22:25:32 +0700

    successfully get user by id and set post by author id

--- server/src/services/user.service.ts
+++ server/src/services/user.service.ts
@@ -1,20 +1,11 @@
-import { Types } from 'mongoose';
 import UserModel from '../models/user.model';
 
 const userService = {
-  getUserById: async (userId: Types.ObjectId) => {
-    if (!userId) {
-      throw new Error('User ID is required');
-    }
+  getUserById: async (userId: string) => {
     // select specific fields to be returned to the frontend
-    const user = await UserModel.findById(userId).select({
-      firstName: 1,
-      lastName: 1,
-      email: 1,
-      profileImage: 1,
-      institution: 1,
-      bio: 1,
-    });
+    const user = await UserModel.findById(userId).select(
+      'firstName lastName email profileImage institution bio'
+    );
 
     return user;
   },
