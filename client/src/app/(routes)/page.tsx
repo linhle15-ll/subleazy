a26a53fcf17e035b6e05fda4cf6591568303f002
Ethan Nguyen <gsthanh@students.cs.ubc.ca>
2025-06-06 19:43:48 -0700

    save for pull origin

--- client/src/app/(routes)/page.tsx
+++ client/src/app/(routes)/page.tsx
@@ -70,19 +70,22 @@ export default function LandingPage() {
         </div>
       </section>
 
-        {/* Featured Listings Section */}
+      {/* Featured Listings Section */}
       <section className="px-6 lg:px-12">
-        {(loading) ? (
-          <div> <Loading /> </div>
-        ) : (error) ? (
-            <div>Error: {error}</div>
-        ) : (posts && posts.length > 0) ? (
-          <PostingGrid
-            isVertical={true}
-            posts={posts}
-          />
-        ) : (<div className="font-medium text-2xl text-grey">No posts available</div>)}
-        
+        {loading ? (
+          <div>
+            {' '}
+            <Loading />{' '}
+          </div>
+        ) : error ? (
+          <div>Error: {error}</div>
+        ) : posts && posts.length > 0 ? (
+          <PostingGrid isVertical={true} posts={posts} />
+        ) : (
+          <div className="font-medium text-2xl text-grey">
+            No posts available
+          </div>
+        )}
       </section>
     </div>
   );
