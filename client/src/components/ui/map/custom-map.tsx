a395ab245f16a1c5a24f141a2d2b9352c6271162
Duc Nguyen <ngmductm@gmail.com>
2025-06-09 18:48:33 +0700

    add components for a functional map in the search page

---
+++ client/src/components/ui/map/custom-map.tsx
@@ -0,0 +1,61 @@
+import { useSortStore } from '@/lib/stores/sort.store';
+import { Post } from '@/lib/types/post.types';
+import {
+  AdvancedMarker,
+  Map,
+  MapCameraChangedEvent,
+  MapCameraProps,
+} from '@vis.gl/react-google-maps';
+import { ElementType, useEffect, useState } from 'react';
+
+export function CustomMap({
+  marker: Marker,
+  posts,
+  className,
+}: {
+  marker: ElementType;
+  posts: Post[];
+  className?: string;
+}) {
+  const places = useSortStore((state) => state.places);
+  const center = useSortStore((state) => state.center);
+  const [cameraProps, setCameraProps] = useState<MapCameraProps>({
+    center: { lat: 38.907, lng: -77.036 },
+    zoom: 12,
+  });
+
+  useEffect(() => {
+    if (posts.length == 0 || !center.lat || !center.lng) return;
+
+    const { lat, lng } = center;
+
+    setCameraProps({
+      ...cameraProps,
+      center: { lat, lng },
+    });
+  }, [center]);
+
+  const handleCameraChange = (e: MapCameraChangedEvent) =>
+    setCameraProps(e.detail);
+
+  return (
+    posts?.length > 0 && (
+      <Map
+        mapId={'12b033e06f8b8b2463b91f1e'}
+        {...cameraProps}
+        onCameraChanged={handleCameraChange}
+        className={className}
+      >
+        {posts.map((post, index) => (
+          <Marker key={index} post={post} />
+        ))}
+        {places.map((place, index) => (
+          <AdvancedMarker
+            position={{ lat: place.lat, lng: place.lng }}
+            key={index}
+          />
+        ))}
+      </Map>
+    )
+  );
+}
