2b43419c2ead9aba775ac30975f8c5c894d4e042
Duc Nguyen <ngmductm@gmail.com>
2025-06-13 12:45:50 +0700

    remove unnecessary stuff

--- server/src/routes/house.routes.ts
+++
@@ -1,8 +0,0 @@
-// import { Router } from 'express';
-// import houseController from '../controllers/house.controller';
-
-// const router = Router();
-
-// router.post('/create', houseController.createHouse);
-
-// export default router;
