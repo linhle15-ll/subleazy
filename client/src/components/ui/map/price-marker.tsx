f844776f13e1091730ea3689759958a363edc027
Duc Nguyen <ngmductm@gmail.com>
2025-06-27 23:31:28 +0700

    update posting marker on search page

--- client/src/components/ui/map/price-marker.tsx
+++ client/src/components/ui/map/price-marker.tsx
@@ -7,6 +7,7 @@ import {
   InfoWindow,
   useAdvancedMarkerRef,
 } from '@vis.gl/react-google-maps';
+import Image from 'next/image';
 import { useState } from 'react';
 
 export function PriceMarker({ post }: { post: Post }) {
@@ -21,6 +22,13 @@ export function PriceMarker({ post }: { post: Post }) {
         collisionBehavior={CollisionBehavior.OPTIONAL_AND_HIDES_LOWER_PRIORITY}
         onMouseEnter={() => setInfoShown(true)}
         onMouseLeave={() => setInfoShown(false)}
+        onClick={() =>
+          window.open(
+            `${window.location.origin}/posting?id=${post._id}`,
+            '_blank',
+            'noopener,noreferrer'
+          )
+        }
       >
         <div className="bg-white border border-gray-300 shadow-md p-2 font-medium">
           ${post.price}
@@ -29,6 +37,13 @@ export function PriceMarker({ post }: { post: Post }) {
       {infoShown && (
         <InfoWindow anchor={marker} headerDisabled={true}>
           <div className="bg-white font-medium">{post.title}</div>
+          <Image
+            src={post.media[0]}
+            alt={post.title}
+            width={200}
+            height={200}
+            className="object-cover mt-1"
+          />
         </InfoWindow>
       )}
     </>
