564f8cac966cb17931756d31b9bfeff9425aa86e
xuananh-tran <189546946+xuananh-tran@users.noreply.github.com>
2025-07-08 01:33:25 -0700

    Add lifestyle store and frontend services

--- client/src/services/user.service.ts
+++ client/src/services/user.service.ts
@@ -1,7 +1,7 @@
 import { Result } from '@/lib/types/common.types';
-import { User } from '@/lib/types/user.types';
-import api from './api';
+import { Lifestyle, User } from '@/lib/types/user.types';
 import { AxiosError } from 'axios';
+import api from './api';
 
 const userService = {
   getUserById: async (id: string): Promise<Result<User>> => {
@@ -37,6 +37,47 @@ const userService = {
           (error as AxiosError<{ error: string }>).response?.data.error ||
           JSON.stringify((error as AxiosError<{ error: string }>).response?.data) ||
           'Failed to get posts by user id', 
+      };
+    }
+  },
+
+  createOrEditLifestyle: async (
+    id: string,
+    submissionData: Lifestyle
+  ): Promise<Result<User>> => {
+    try {
+      console.log('Submitting lifestyle:', submissionData);
+      const response = await api.patch(
+        `/users/${id}/lifestyle`,
+        submissionData
+      );
+      return {
+        success: true,
+        data: response.data,
+      };
+    } catch (error) {
+      return {
+        success: false,
+        error:
+          (error as AxiosError<{ error: string }>).response?.data.error ||
+          'Failed to edit lifestyle form',
+      };
+    }
+  },
+
+  getLifestyle: async (id: string): Promise<Result<User>> => {
+    try {
+      const response = await api.get(`/users/${id}/lifestyle`);
+      return {
+        success: true,
+        data: response.data,
+      };
+    } catch (error) {
+      return {
+        success: false,
+        error:
+          (error as AxiosError<{ error: string }>).response?.data.error ||
+          'Failed to get lifestyle form',
       };
     }
   },
