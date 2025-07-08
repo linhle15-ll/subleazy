59d518b60c3a3de9a3c5d354d4a82787e32876c9
Ngoc Linh Le <ngoclinhle@Ngocs-MacBook-Pro.local>
2025-07-08 07:17:41 +0700

    BE for wishlist

--- server/src/controllers/wish.controller.ts
+++ server/src/controllers/wish.controller.ts
@@ -1,37 +1,63 @@
 import { NextFunction, Request, Response } from 'express';
-import { Types } from 'mongoose';
 import wishService from '../services/wish.service';
 import { Wish } from '../types/wish.types';
+import Post from '../models/post.model';
+import mongoose, { Types } from 'mongoose';
 import { getAuthRequest } from '../utils/common.utils';
-import postService from '../services/post.service';
-import { PostStatus } from '../types/enums';
 
 const wishController = {
   createWish: async (req: Request, res: Response, next: NextFunction) => {
     try {
       const authReq = getAuthRequest(req);
       const data: Wish = authReq.body;
-      data.user = new Types.ObjectId(authReq.user.id);
+      data.author = new Types.ObjectId(authReq.user.id);
 
-      if (!data.post) {
-        res.status(400).json({ error: 'Missing post' });
+      if (!data.post || !data.author) {
+        res.status(400).json({ error: 'Missing post or user' });
         return;
       }
 
       // post can be added to wish list only if it is not closed
-      const post = await postService.getPost(data.post.toString());
-      if (!post) {
+      const postDoc = await Post.findById(data.post);
+      if (!postDoc) {
         res.status(404).json({ error: 'Post not found.' });
         return;
       }
 
-      if (post.status === PostStatus.CLOSED) {
+      if (postDoc && postDoc.status === 'closed') {
         res.status(400).json({ error: 'Post is unavailable.' });
         return;
       }
 
       const wish = await wishService.createWish(data);
       res.status(201).json(wish);
+    } catch (error) {
+      next(error);
+    }
+  },
+  getWishListByUserId: async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    const authReq = getAuthRequest(req);
+
+    try {
+      const userId = authReq.params.id;
+
+      if (!userId || !mongoose.isValidObjectId(userId)) {
+        res.status(400).json({ error: 'Invalid user ID' });
+        return;
+      }
+
+      const wishList = await wishService.getWishListByUserId(userId);
+
+      if (!wishList || (Array.isArray(wishList) && wishList.length === 0)) {
+        res.status(200).json([]);
+        return;
+      }
+
+      res.status(200).json(wishList);
     } catch (error) {
       next(error);
     }
