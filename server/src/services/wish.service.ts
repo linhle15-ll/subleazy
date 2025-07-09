fd831d302e4507f616d4803b13105d0dd892a033
xuananh-tran <189546946+xuananh-tran@users.noreply.github.com>
2025-07-09 03:56:40 -0700

    Add getInterestedUsersByPost to wish service

--- server/src/services/wish.service.ts
+++ server/src/services/wish.service.ts
@@ -3,16 +3,25 @@ import { Wish } from '../types/wish.types';
 
 const wishService = {
   createWish: async (data: Wish) => {
-    const wish = await wishModel.create({ user: data.author, post: data.post });
+    const wish = await wishModel.create({ user: data.user, post: data.post });
     return wish;
   },
 
   getWishListByUserId: async (userId: string) => {
     const wishes = await wishModel
-      .find({ author: userId })
+      .find({ user: userId })
       .populate('post')
-      .populate('author');
+      .populate('user');
     return wishes;
+  },
+
+  getInterestedUsersByPost: async (postId: string) => {
+    const wish = await wishModel.find({ post: postId }).populate({
+      path: 'user',
+      select: 'firstName lastName profileImage lifestyle lifestyleVector',
+      options: { lean: true },
+    });
+    return wish.map((w) => w.user);
   },
 };
 
