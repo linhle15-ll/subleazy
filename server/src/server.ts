89cf957c36d7c2330e03c1f3feddf444522d0206
Duc Nguyen <ngmductm@gmail.com>
2025-07-10 21:58:21 +0700

    set up socket.io

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,12 +5,25 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import app from './app';
+import { Server } from 'socket.io';
 
 // Define the port
 const PORT = process.env.PORT || 5001; // if there is no port, return port 5001
 
 // create server instance
 const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  },
+});
+
+// socket.io connection
+io.on('connection', (socket) => {
+  console.log('a user connected ' + socket.id);
+  socket.on('disconnect', () => console.log('user disconnected'));
+});
 
 server.listen(PORT, () => {
   console.log(`Server started on port ${PORT}.`);
