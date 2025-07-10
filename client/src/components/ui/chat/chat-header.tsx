26c633d737dfd5e28ad97656cd2d15585a4abc26
Duc Nguyen <ngmductm@gmail.com>
2025-07-10 15:20:26 +0700

    add chat info sidebar

--- client/src/components/ui/chat/chat-header.tsx
+++ client/src/components/ui/chat/chat-header.tsx
@@ -1,12 +1,19 @@
 import { Ellipsis } from 'lucide-react';
 
-export default function ChatHeader({ groupName }: { groupName: string }) {
+export default function ChatHeader({
+  groupName,
+  onClick,
+}: {
+  groupName: string;
+  onClick: () => void;
+}) {
   return (
     <div className="flex justify-between items-center p-2">
       <div title={groupName} className="text-2xl font-medium truncate">
         {groupName}
       </div>
       <button
+        onClick={onClick}
         className="p-2 rounded-full hover:bg-gray-100"
         title={'More information'}
         aria-label={'More information'}
